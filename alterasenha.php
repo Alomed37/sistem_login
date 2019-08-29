d4f4a4301681eb2842ba03d86826da37a3d3b8d5
Alomed37 <54580683+Alomed37@users.noreply.github.com>
2019-08-29 09:20:43 -0300

    update 002

--- alterasenha.php
+++ alterasenha.php
@@ -1,8 +1,14 @@
+<?php
+require_once 'classes/usuarios.php';
+$u = new Usuario; //herda da classe acima transforma em variavel
+?>
+
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="css/estilo.css">
-	<!--	<title>Alterar Senha</title>   -->
+	<title>Alterar Senha</title>
 </head>
 
 <body>
@@ -12,7 +18,7 @@
 
 			<input type="password" name="novasenha" placeholder="Senha" maxlength="20">
 			<input type="password" name="confSenha" placeholder="Confirmar senha">
-			<input type="submit" value="Cadastrar" class="entrar">
+			<input type="submit" value="Trocar senha" class="entrar">
 
 		</form>
 	</div>
@@ -25,36 +31,37 @@
 
 <?php
 
-require_once 'usuarios.php';
-$u = new Usuario; 
 
 if (isset($_POST['novasenha'])) {
-
-
 
 	$novasenhaup = htmlentities(addslashes($_POST['novasenha']));
 	$confirmarSenha = htmlentities(addslashes($_POST['confSenha']));
 
-
-	//verificando se todos os campos nao estao vazios
 	if (!empty($novasenhaup) && !empty($confirmarSenha)) {
+
 		$u->conectar("sistem_login", "localhost", "root", "");
-		if ($u->msgErro == "") //conectado normalmente;
-			{
-				if ($novasenhaup == $confirmarSenha) {
-					if ($u->trocarSenha($novasenhaup, $id_usuario)) {
-						echo '<br>';
-						echo "Cadastro realizado com sucesso!";
-					} else {
-						echo '<br>';
-						echo "Senha não conferem.";
-					}
+		if ($u->msgErro == "") {
+
+
+			if ($novasenhaup == $confirmarSenha) {
+				if ($update = $u->trocarSenha($novasenhaup, $id_usuario)) {
+					echo '<br>';
+					echo "Cadastro realizado com sucesso!";
 				}
+			} else {
+				echo '<br>';
+				echo "Senhas não conferem!";
 			}
+		} else {
+			echo "Erro: " . $u->msgErro;
+		}
+	} else {
+		echo "Preencha todos os campos!";
 	}
 }
-?>
-</div>
-</body>
 
-</html>
+//var_dump($novasenhaup);
+//var_dump($confirmarSenha);
+//var_dump ($id_usuario);
+
+?>
